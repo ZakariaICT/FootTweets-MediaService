cb77b22bc5cc311f24acb162735e56f6e4e6dc81
Hassan,Zakaria <z.hassan@student.fontys.nl>
2023-12-11 11:08:01 +0100

    X

--- MediaService/Program.cs
+++ MediaService/Program.cs
@@ -29,17 +29,15 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-    //app.UseSwagger();
-    //app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Media API V1");
     });
+}
 
-//}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
